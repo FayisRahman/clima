96171edd2b8b94aa995809bd3e1b25cb4a546011
fayisRahman <we.are.thadathil@gmail.com>
2024-10-06 12:49:51 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
-import 'package:clima/screens/city_screen.dart';
-import 'package:clima/screens/location_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,13 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      routes: {
-        '/': (context) => LoadingScreen(),
-        '/first': (context) => CityScreen(),
-        '/second': (context) => LocationScreen(),
-      },
+      home: LoadingScreen(),
     );
   }
 }
